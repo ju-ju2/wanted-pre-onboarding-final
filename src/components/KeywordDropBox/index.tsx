eb6f30380b08e8e2862e1b3b5e8aca2c13dd3374
한주연 <hjy2859@naver.com>
2023-07-17 22:27:44 +0900

    feat: 키워드 검색결과 map 처리

--- src/components/KeywordDropBox/index.tsx
+++ src/components/KeywordDropBox/index.tsx
@@ -1,9 +1,39 @@
-import { SearchTitle, Wrapper } from "./styled";
+import { useEffect } from "react";
+import {
+  CommendedList,
+  EmptyText,
+  ListItem,
+  RowList,
+  SearchIcon,
+  SearchTitle,
+  Wrapper,
+} from "./styled";
 
-export default function KeywordDropBox() {
+interface SickData {
+  sickCd: string;
+  sickNm: string;
+}
+interface SickDataProps {
+  list: SickData[];
+}
+export default function KeywordDropBox({ list }: SickDataProps) {
   return (
-    <Wrapper>
-      <SearchTitle>추천 검색어</SearchTitle>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <SearchTitle>추천 검색어</SearchTitle>
+        {list.length !== 0 ? (
+          <CommendedList>
+            {list.map((el) => (
+              <RowList>
+                <SearchIcon />
+                <ListItem key={el.sickCd}>{el.sickNm}</ListItem>
+              </RowList>
+            ))}
+          </CommendedList>
+        ) : (
+          <EmptyText>추천 검색어가 없습니다.</EmptyText>
+        )}
+      </Wrapper>
+    </>
   );
 }
