ed840afadc954ed2758a6fff63d62a525f4c0b67
한주연 <hjy2859@naver.com>
2023-07-17 23:11:47 +0900

    feat: 캐싱 처리 구현

--- src/apis/getSickApi.ts
+++ src/apis/getSickApi.ts
@@ -13,8 +13,14 @@ const cache: CacheType = {};
 
 export const getSickApi = async (keyword: string) => {
   try {
+    if (cache[keyword]) {
+      console.log("캐싱된 데이터");
+      return cache[keyword];
+    }
     const res = await axios.get(`http://localhost:4000/sick?q=${keyword}`);
     const data = res.data;
+    console.log("캐싱이 안된 새 데이터");
+    cache[keyword] = data;
     return data;
   } catch (err) {
     console.log(err);
