a2c9a1c7675e50edd06ee89122fdccca0a129f73
한주연 <hjy2859@naver.com>
2023-07-17 23:11:27 +0900

    feat: 캐시 타입 설정

--- src/apis/getSickApi.ts
+++ src/apis/getSickApi.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+interface CacheData {
+  sickCd: string;
+  sickNm: string;
+}
+
+interface CacheType {
+  [key: string]: CacheData[];
+}
+
+const cache: CacheType = {};
+
 export const getSickApi = async (keyword: string) => {
   try {
     const res = await axios.get(`http://localhost:4000/sick?q=${keyword}`);
