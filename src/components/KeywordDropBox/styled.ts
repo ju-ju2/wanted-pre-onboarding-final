223e310394b36986630b1af2ed1bcd1b6cac7315
한주연 <hjy2859@naver.com>
2023-07-17 22:29:05 +0900

    style: 스타일 설정

--- src/components/KeywordDropBox/styled.ts
+++ src/components/KeywordDropBox/styled.ts
@@ -1,3 +1,4 @@
+import { SearchOutlined } from "@ant-design/icons";
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
@@ -10,12 +11,42 @@ export const Wrapper = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  padding: 10px;
+  padding: 15px;
   position: absolute;
   left: 0;
   top: 60px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
 `;
 
 export const SearchTitle = styled.p`
   color: grey;
+  margin: 10px 0;
+`;
+
+export const CommendedList = styled.ul`
+  list-style: none;
+`;
+
+export const ListItem = styled.li`
+  list-style: none;
+  margin: 10px 0;
+  font-size: 18px;
+  color: black;
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+export const SearchIcon = styled(SearchOutlined)`
+  color: black;
+`;
+export const RowList = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+export const EmptyText = styled.p`
+  color: black;
+  margin-top: 10px;
 `;
