9ebe6813cbfe31f5bd7120b1c5c59a4d0a99eaff
한주연 <hjy2859@naver.com>
2023-07-18 12:34:36 +0900

    feat: useFocusInput 커스텀 훅 구현으로 방향키로 검색어 추천 이동

---
+++ src/hooks/useFocusIndex.ts
@@ -0,0 +1,22 @@
+import { KeyboardEvent, useState } from "react";
+
+export const useFocusIndex = (maxLength: number) => {
+  const [focusIndex, setFocusIndex] = useState(-1);
+
+  const changeFocusIndex = (e: KeyboardEvent<HTMLInputElement>) => {
+    console.log(e.key);
+    if (e.key === "ArrowDown") {
+      focusIndex >= maxLength - 1
+        ? setFocusIndex(-1)
+        : setFocusIndex((prev) => prev + 1);
+    }
+    if (e.key === "ArrowUp") {
+      e.preventDefault(); // 커서가 왼쪽으로 위치하는 것 방지
+      focusIndex <= -1
+        ? setFocusIndex(maxLength - 1)
+        : setFocusIndex((prev) => prev - 1);
+    }
+  };
+
+  return { changeFocusIndex, focusIndex, setFocusIndex };
+};
